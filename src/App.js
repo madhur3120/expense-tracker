3bd5c364cb514413da0cf46001a0b1100da145bd
Madhur Saxena <saxenamadhur96@gmail.com>
2022-09-17 14:24:58 +0530

    patch-1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import AddExpense from "./pages/add-expense";
+import Home from "./pages/home/index";
 
-
-function App() {
+const App = () => {
   return (
-    <div>hello world</div>
+    <Router>
+      <Header />
+
+      <Route path="/" element={Home} />
+      <Route path="/add-expense" element={AddExpense} />
+
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
